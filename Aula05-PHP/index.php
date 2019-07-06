e1380c9dfcac1e13c826e255bd5ec73e0f134f30
pollytakisita <polianarodrigues.eng@outlook.com>
2019-07-06 12:10:38 -0300

    remover arquivo e pasta

--- Aula05-PHP/index.php
+++ Aula05-PHP/index.php
@@ -1,91 +1,109 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Resolução - HTTP</title>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
-    <style>
-        li{
-            margin-bottom: 20px;
-        }
-    </style>
-</head>
+<?php require_once("./inc/head.php") ?>
+
 <body>
+
+  <!-- variaveis de conteudo -->
+  <?php
+    $cursos = [
+      "Full Stack" => ["full.jpeg", "Curso de desenvolvimento web", 41.99, "full-stack"],
+      "Marketing Digital" => ["marketing.jpg", "Curso de marketing", 51.99, "marketing"],
+      "User Experience" => ["ux.png", "Curso de Experiência de Usuário", 71.99, "ux"],
+      "Mobile Android" => ["android.png", "Curso de desenvolvimento android", 0.99, "android"]
+    ];
+
+    $usuario = [
+      "nome" => "Fulano",
+      "email" => "fulano@digitalhouse.com",
+      "senha" => "1234",
+      "nivel_acesso" => mt_rand(0,1)
+    ];
+  ?>
+
+  <main>
+    <?php require_once("./inc/navbar.php"); ?>
+    <!-- produtos -->
     <div class="container">
-        <section>
-            <ol> 
-                <li>
-                    <!-- Pergunta 1 -->
-                    Crie uma página com o seguinte formulário:<br><br>
-                    <!-- Resposta 1 -->
-                    <form action="exibir.php" method="POST">
+      <div class="row">
+        <!-- cursos -->
+        <?php foreach($cursos as $nome => $infos){ ?>
+          <div class="col-xs-4 col-sm-6 col-md-6 col-lg-6">
+            <div class="thumbnail">
+              <img src="<?php echo 'assets/img/' . $infos[0]; ?>" alt="Full Stack">
+              <div class="caption">
+                <h3><?php echo $nome; ?></h3>
+                <p><?php echo $infos[1]; ?></p>
+                <p><strong><?php echo $infos[2]; ?></strong></p>
+                <a href="#" class="btn btn-primary" role="button" 
+                    data-toggle="modal" data-target="<?php echo "#$infos[3]"; ?>">Comprar
+                </a>
+              </div>
+            </div>
+          </div>
+        <?php } ?>
+	 <!-- área de compra  -->
+              <?php foreach ($cursos as $nome => $infos): ?>
+                <div class="modal fade" id="<?php echo $infos[3] ?>" role="dialog">
+                    <div class="modal-dialog">
+                        <div class="modal-content">
+                            <div class="modal-header">
+                                <button type="button" class="close" data-dismiss="modal">&times;</button>
+                                <h4 class="modal-title">Digite seus dados para efetuar a compra</h4>
+                            </div>
+                            <div class="modal-body">
+                                <h4>Curso de
+                                    <?php echo $nome ?>.</h4>
+                                <h4>Valor: R$
+                                    <?php echo $infos[2] ?>
+                                </h4>
+                      <form class="" action="validarCompra.php" method="POST">
+                          <input type="hidden" name="nomeCurso" value="<?php echo $nome; ?>">
+                          <input type="hidden" name="precoCurso" value="<?php echo $infos[2]; ?>">
+                          <div class="input-group col-md-5">
+                              <label for="labelNome">Nome Completo</label>
+                              <input id="labelNome" type="text" name="nomeCompleto" class="form-control"
+                                  placeholder="Digite Aqui...">
+                          </div>
 
-                        Nome: <input type="text" name="nome"><br><br>
+                          <div class="input-group col-md-5">
+                              <label for="labelCpf">CPF</label>
+                              <input id="labelCpf" type="number" name="cpf" class="form-control"
+                                  placeholder="Digite Aqui...">
+                          </div>
 
-                        E-mail: <input type="text" name="email"><br><br>
+                          <div class="input-group col-md-5">
+                              <label for="labelNroCartao">Número do Cartão</label>
+                              <input id="labelNroCartao" type="number" name="numeroCartao" class="form-control"
+                                  placeholder="Digite Aqui...">
+                          </div>
 
-                        Como você ficou sabendo desta informação ?<br>
-                        Google <input type="checkbox" name="informacao[]" value="google">
-                        Rádio <input type="checkbox" name="informacao[]" value="radio">
-                        Amigo <input type="checkbox" name="informacao[]" value="amigo"><br><br>
-                        
-                        Informe seu sexo:<br>
-                        Masculino <input type="radio" name="sexo" value="masculino">
-                        Feminino <input type="radio" name="sexo" value="feminino">
-                        Outro <input type="radio" name="sexo" value="outros"><br><br>
+                          <div class="input-group col-md-5">
+                              <label for="labelValidade">Validade</label>
+                              <input id="labelValidade" type="month" name="validade" class="form-control"
+                                  placeholder="Digite Aqui...">
+                          </div>
 
-                        <input type="checkbox" name="termos" value="termos"> Termos e Condições<br><br>
-                        <input type="submit">
-                    </form>
-                </li>
-            </ol>
-            <ol start="3">
-                <li>
-                    <!-- Pergunta 3 -->
-                    Modificar o formulário para que ele envie os dados através de <a href="https://secure.php.net/manual/pt_BR/reserved.variables.post.php">$_POST</a>. Como deveríamos modificar <b>exibir.php</b> para que continue funcionando?<br><br>
-                    <!-- Resposta 3 -->
-                    R: Quando alterado o método de envio dos dados, devemos alterar também no arquivo que irá tratar essas informações, no caso
-                    também iremos obter a diferença no envio, pois,  uma requisição <b>$_GET</b> é enviada como string anexada a URL e é menos segura,
-                    enquanto que a requisição <b>$_POST</b> é encapsulada junto ao corpo da requisição HTTP, não pode ser vista na URL e é mais segura.
-                </li>
-                <li>
-                    <!-- Pergunta 4 -->
-                    Adicionar ao formulário os seguintes inputs:<br><br>
+                          <div class="input-group col-md-5">
+                              <label for="labelCvv">CVV</label>
+                              <input id="labelCvv" type="number" name="cvv" class="form-control"
+                                  placeholder="Digite Aqui...">
+                          </div>
 
-                    <!-- Pergunta 4 - a -->
-                    a. Adicionar uma <a href="https://www.w3schools.com/tags/tryit.asp?filename=tryhtml5_input_type_checkbox">checkbox</a> com opções, que responda à pergunta “Como você ficou sabendo deste site?”. Opções: “Google”, “Rádio”, “Indicação de Amigo”.<br>
-                    <!-- Resposta 4 - a -->
-                    R: O campo foi adicionado ao formulário da questão 1.<br><br>
-
-                    <!-- Pergunta 4 - b -->
-                    b. Adicionar um <a href="https://www.w3schools.com/html/tryit.asp?filename=tryhtml_radio">radio button</a> para selecionar o sexo.<br>
-                    <!-- Resposta 4 - b -->
-                    R: O campo foi adicionado ao formulário da questão 1.<br><br>
-
-                    <!-- Pergunta 4 - c -->
-                    c. Adicionar uma <a href="https://www.w3schools.com/tags/tryit.asp?filename=tryhtml5_input_type_checkbox">checkbox</a> para aceitar “Termos e Condições”.<br>
-                    <!-- Resposta 4 - c -->
-                    R: O campo foi adicionado ao formulário da questão 1.<br><br>
-
-                    <!-- Pergunta 4 - d -->
-                    d. Na página <b>exibir.php</b> imprima os valores selecionados no formulário.
-                    
-                </li>
-                <li>
-                    <!-- Pergunta 5 -->
-                    Testar a função de php <a href="https://secure.php.net/manual/pt_BR/function.getallheaders.php">getAllHeaders()</a> e imprimir o resultado. 
-                    O que encontramos? Imprima todos os valores usando foreach, apenas para você conhecê-los.<br><br>
-                    <!-- Resposta 5 -->
-                    <?php
-                        foreach (getallheaders() as $name => $value) {
-                            echo "$name: $value <br>";
-                        }
-                    ?>
-                </li>
-            </ol>
-        </section>
+                          <button type="submit" name="button" class="btn btn-info">Comprar</button>
+                      </form>
+                            </div>
+                            <div class="modal-footer">
+                                <p align="center">Copyright &copy; Cursos 2019</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+              <?php endforeach; ?>
+	          <!-- fim área da compra -->
+      	</div>
     </div>
+  </main>
+  
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js" charset="utf-8"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
 </body>
 </html>
